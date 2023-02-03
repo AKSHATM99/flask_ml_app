b0365758cebac53d3454e1f9f513feba615e6abd
AKSHATM99 <mishra.akshat27sep@gmail.com>
2023-02-03 23:48:30 +0530

    New Changes

--- app.py
+++ app.py
@@ -2,28 +2,26 @@ from flask import *
 import os
 import numpy as np
 import pickle
-# from tensorflow import keras
-# from tensorflow.keras.preprocessing import image
+from tensorflow import keras
+from tensorflow.keras.preprocessing import image
+
 
 UPLOAD_FOLDER = 'static/files/'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-
 app = Flask(__name__)
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 #Loading & Compiling Model 
-# model = keras.models.load_model('MLModel.h5')
-# model.compile(loss='binary_crossentropy',
-#              optimizer = 'adam',
-#              metrics=['accuracy']
-#              )
+model = keras.models.load_model('BikeVsCar_Model.h5') # 90.50% Accurate
+model.compile(loss='binary_crossentropy',
+             optimizer = 'adam',
+             metrics=['accuracy']
+             )
 
 #Loading Linear Regression Model
-# filename = 'linear_reg_model.pkl'
 reg = pickle.load(open('linear_reg_model.pkl', 'rb'))
 
 #Route TO HomePage
-
 @app.route("/")
 @app.route("/home")
 def home():           
@@ -40,34 +38,32 @@ def reg_class():
     return render_template("linear_reg.html")
 
 #Login Page Route
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    return render_template('login.html')
 
 #Signup Page Route
-@app.route("/signup")
+@app.route("/signup", methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
-
+    return render_template('signup.html')
 
 #Upload For Image Classification
 @app.route("/upload", methods=['POST'])
 def upload():
     file = request.files['myfile']
-    # filename = secure_filename(file.filename)
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
     # Transforming Image to array for feeding to model
-    img = image.load_img("static/files/{}".format(file.filename),target_size=(200, 200))
+    img = image.load_img("static/files/{}".format(file.filename),target_size=(100, 100))
     x = image.img_to_array(img)
     x = np.expand_dims(x,axis=0)
     images = np.vstack([x])
-    val =(model.predict(images) > 0.5)*1 
-    if val==1:
+    val =(model.predict(images))
+    if val<0.5:
         os.remove("static/files/{}".format(file.filename))
-        return render_template("image_model.html", lable="Class 1")
+        return render_template("image_model.html", lable="Bike")
     else:
         os.remove("static/files/{}".format(file.filename))
-        return render_template("image_model.html", lable="Class 2")  
+        return render_template("image_model.html", lable="Car")  
 
 #Upload For Linear Regression
 @app.route("/predict", methods=['POST'])
